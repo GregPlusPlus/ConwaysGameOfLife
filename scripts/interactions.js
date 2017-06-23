70ffa71ca34fd88e45f0d1a5e6197fc14ec4b840
GregPlusPlus <bostgregoire@gmail.com>
2017-06-23 17:24:31 +0200

    Update interactions.js

--- scripts/interactions.js
+++ scripts/interactions.js
@@ -1,49 +1,53 @@
+// This function is called wen 'click' event is fired
 function interact(event) {
-	var canvas = document.getElementById("canvas");
-	var r = Math.floor((event.clientY - canvas.getBoundingClientRect().top) / 10);
-	var c = Math.floor((event.clientX - canvas.getBoundingClientRect().left) / 10);
+	var canvas = document.getElementById("canvas"); // Gathering 'canvas' element
+	var r = Math.floor((event.clientY - canvas.getBoundingClientRect().top) / 10); // Gathering row
+	var c = Math.floor((event.clientX - canvas.getBoundingClientRect().left) / 10); // Gathering column
 
-	grid[r][c].alive = !grid[r][c].alive;
+	grid[r][c].alive = !grid[r][c].alive; // Toggling cell's life state
 
-	ctx.clearRect(0, 0, columns * 10, rows * 10);
+	ctx.clearRect(0, 0, columns * 10, rows * 10); // Clear canvas
 
-	for(var i = 0 ; i < rows ; i++) {
-		for(var j = 0 ; j < columns ; j++) {
-			grid[i][j].countNeighbors(columns, rows, grid);
+	for(var i = 0 ; i < rows ; i++) {		// |- Scanning cells
+		for(var j = 0 ; j < columns ; j++) {	// |
+			grid[i][j].countNeighbors(columns, rows, grid); // Updating neighbors
 		}
 	}
 
-	for(var i = 0 ; i < rows ; i++) {
-		for(var j = 0 ; j < columns ; j++) {
-			grid[i][j].show();
+	for(var i = 0 ; i < rows ; i++) {		// |- Scanning cells
+		for(var j = 0 ; j < columns ; j++) {	// |
+			grid[i][j].show(); // Dispay cell
 		}
 	}
 
-	countPopulation();
+	countPopulation(); // Update population
 }
 
+// This function resets the game
 function reset() {
-	makeGrid(document.getElementById("PRange").value);
+	makeGrid(document.getElementById("PRange").value); // Creating a new grid
 	iterations = 0;
 	document.getElementById("iterations").innerHTML = "Iterations: " + iterations;
-	pause();
+	pause(); // Pause game
 }
 
+// This function clears the grid (removes all alive cells)
 function clearGrid() {
-	document.getElementById("PRange").value = 0;
-	reset();
+	document.getElementById("PRange").value = 0; // Set randomization level to 0
+	reset(); // Reset the game
 }
 
+// This function pauses the game
 function pause() {
-		clearInterval(intervalId);
+		clearInterval(intervalId); // Removing the interval
 		intervalId = undefined;	
 }
 
+// This function Starts/Pauses/Resumes the game
 function toggle() {
-	if(intervalId == undefined)
-	{
-		start();
+	if(intervalId == undefined) {
+		start(); // Start/Resume the game
 	} else {
-		pause();
+		pause(); // Pause the game
 	}
 }
