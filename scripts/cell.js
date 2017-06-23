fd753b1f1a4a4a43b62f923f351f238d43e4587a
GregPlusPlus <bostgregoire@gmail.com>
2017-06-23 15:33:41 +0200

    Update cell.js

--- scripts/cell.js
+++ scripts/cell.js
@@ -7,64 +7,52 @@ function Cell(r, c, p) {
 	this.wasAlive = false;
 	this.neighbors = 0;
 
-	//This function shows the cell into the canvas according to his coordinates, and his living state
+	// This function displays the cell into the canvas according to his coordinates, and his living state
 	Cell.prototype.show = function() {
-		if(this.alive && this.wasAlive) // The cell stays alive
+		ctx.beginPath();
+		ctx.lineWidth = "1";
+		
+		if(this.alive && this.wasAlive) 		// The cell stays alive
 		{
-			ctx.beginPath();
-			ctx.lineWidth = "1";
 			ctx.strokeStyle = "#272822";
 			ctx.fillStyle = "#FD971F";
-			ctx.rect(this.column * 10, this.row * 10, 10, 10);
-			ctx.stroke();
-			ctx.fill();
-		} else if(this.alive && !this.wasAlive) { // The cell borns
-			ctx.beginPath();
-			ctx.lineWidth = "1";
+		} else if(this.alive && !this.wasAlive) { 	// The cell borns
 			ctx.strokeStyle = "#272822";
 			ctx.fillStyle = "#A6E22E";
-			ctx.rect(this.column * 10, this.row * 10, 10, 10);
-			ctx.stroke();
-			ctx.fill();
-		} else if(!this.alive && this.wasAlive) { // The cell dies
-			ctx.beginPath();
-			ctx.lineWidth = "1";
+		} else if(!this.alive && this.wasAlive) { 	// The cell dies
 			ctx.strokeStyle = "#272822";
 			ctx.fillStyle = "#66D9EF";
-			ctx.rect(this.column * 10, this.row * 10, 10, 10);
-			ctx.stroke();
-			ctx.fill();
-		} else { // The cell stays die
-			ctx.beginPath();
-			ctx.lineWidth = "1";
+		} else { 					// The cell stays ded
 			ctx.strokeStyle = "#272822";
 			ctx.fillStyle = "#272822";
-			ctx.rect(this.column * 10, this.row * 10, 10, 10);
-			ctx.stroke();
-			ctx.fill();
 		}
+		
+		ctx.rect(this.column * 10, this.row * 10, 10, 10);
+		ctx.stroke();
+		ctx.fill();
 
 		this.showNeighborsCount(); // Display ne number of neighbors
 	}
 
-	//This function displays the number of neighbors
+	// This function displays the number of neighbors
 	Cell.prototype.showNeighborsCount = function() {
 		ctx.fillStyle = "#5d6052";
 		ctx.font = "10px Arial";
 		ctx.fillText(this.neighbors, this.column * 10 + 2, this.row * 10 - 2 + 10);
 	}
 
+	// This function counts the number of neighbors of the cell according to the number of rows and columns
 	Cell.prototype.countNeighbors = function(cg, rg, grid) {
-		var x = this.column;
-		var y = this.row;
-		this.neighbors = 0;
+		var x = this.column; 	// Only used to write shorter tests/loops
+		var y = this.row;	// Only used to write shorter tests/loops
+		this.neighbors = 0; 	// Reset the number of neighbors
 
-		for(var i = x-1 ; i <= x+1 ; i ++) {
-			for(var j = y-1 ; j <= y+1 ; j ++) {
-				if(i >= 0 && i < cg && j >= 0 && j < rg){
-					if(i != x || j != y){
-						if(grid[j][i].alive) {
-							this.neighbors++;
+		for(var i = x-1 ; i <= x+1 ; i ++) { 				// Scanning cells from x - 1 to x + 1
+			for(var j = y-1 ; j <= y+1 ; j ++) {			// Scanning cells from y - 1 to y + 1
+				if(i >= 0 && i < cg && j >= 0 && j < rg){	// If the scanned cell is inside the array
+					if(i != x || j != y){			// If the scanned cell is not the current cell
+						if(grid[j][i].alive) {		// If the scanned cell is alive
+							this.neighbors++;	// Then, adding 1 to the count
 						}
 					}
 				}
@@ -72,17 +60,18 @@ function Cell(r, c, p) {
 		}
 	}
 
+	// This function updates the living state of the cell
 	Cell.prototype.updateState = function() {
 		this.wasAlive = this.alive;
 
-		if(this.neighbors == 3) {
+		if(this.neighbors == 3) {		// The cell borns
 			this.alive = true;
-		} else if(this.neighbors == 2) {
-			this.alive = this.alive;
-		} else {
+		} else if(this.neighbors == 2) {	// The cell stays alive or dead
+			this.alive = this.alive; // This line has no effect
+		} else {				// The cell dies
 			this.alive = false;
 		}
 
-		this.show();
+		this.show(); // Display the cell
 	}
 }
