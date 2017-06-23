76f223929683aaa38f4b91b8f6686b9c52b0a107
GregPlusPlus <bostgregoire@gmail.com>
2017-06-23 16:31:58 +0200

    Update init.js

--- scripts/init.js
+++ scripts/init.js
@@ -1,24 +1,40 @@
+// This function inits the 2 dimensional array according the number of rows/columns and the randimozation level
 function initGame(r, c, p) {
-	var a = new Array(r);
+	
+	/*         V------- A column
+	 a b c d e f
+	-------------
+	|o|o|o|o|o|o| 1 <-- A row
+	-------------
+	|o|o|o|o|o|o| 2
+	-------------
+	|o|o|o|o|o|o| 3
+	-------------
+	|o|o|o|o|o|o| 4
+	-------------
+	*/
+	
+	var a = new Array(r); // Create a new array in function of the number of rows
 
-	for(var i = 0 ; i < r ; i++) {
-		a[i] = new Array(c);
+	for(var i = 0 ; i < r ; i++) { // Looping through the rows
+		a[i] = new Array(c); // Creating a new row (in function of the number of columns)
 
-		for(var j = 0 ; j < c ; j++) {
-			a[i][j] = new Cell(i, j, p);
+		for(var j = 0 ; j < c ; j++) { // Looping through the columns
+			a[i][j] = new Cell(i, j, p); // Creating a new Cell according the row/column and the randimozation level
 		}
 	}
 
-	for(var i = 0 ; i < rows ; i++) {
-		for(var j = 0 ; j < columns ; j++) {
-			a[i][j].countNeighbors(columns, rows, a);
+	for(var i = 0 ; i < rows ; i++) {		// |-Scanning cells
+		for(var j = 0 ; j < columns ; j++) {	// |
+			a[i][j].countNeighbors(columns, rows, a); // Call the 'countNeighbors' from the current cell
 		}
 	}
 
-	for(var i = 0 ; i < rows ; i++) {
-		for(var j = 0 ; j < columns ; j++) {
-			a[i][j].show();
+	for(var i = 0 ; i < rows ; i++) {		// |-Scanning cells
+		for(var j = 0 ; j < columns ; j++) {	// |
+			a[i][j].show(); // Displaying the current cell
 		}
 	}
-	return a;
+	
+	return a; // Return the 2 dimensional array
 }
