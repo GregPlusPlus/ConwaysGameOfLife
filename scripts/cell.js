41776199866a4389dedd72717dca5b8d1cb9cbed
GregPlusPlus <bostgregoire@gmail.com>
2017-06-23 16:05:13 +0200

    Update cell.js

--- scripts/cell.js
+++ scripts/cell.js
@@ -3,28 +3,25 @@ function Cell(r, c, p) {
 	// Initializing properties
 	this.row = r;
 	this.column = c;
-	this.alive = (Math.random() < p);
+	this.alive = (Math.random() < p); // Define if the cell is alive in function of the randomization level
 	this.wasAlive = false;
 	this.neighbors = 0;
 
 	// This function displays the cell into the canvas according to his coordinates, and his living state
 	Cell.prototype.show = function() {
-		ctx.beginPath();
-		ctx.lineWidth = "1";
+		ctx.beginPath();		// |
+		ctx.lineWidth = "1";		// |- Start drawing
+		ctx.strokeStyle = "#272822"; 	// |
 		
 		// Changing the cell's color according to his living state
 		if(this.alive && this.wasAlive) {		// The cell stays alive
-			ctx.strokeStyle = "#272822";
-			ctx.fillStyle = "#FD971F";
+			ctx.fillStyle = "#FD971F"; // Orange
 		} else if(this.alive && !this.wasAlive) { 	// The cell borns
-			ctx.strokeStyle = "#272822";
-			ctx.fillStyle = "#A6E22E";
+			ctx.fillStyle = "#A6E22E"; // Green
 		} else if(!this.alive && this.wasAlive) { 	// The cell dies
-			ctx.strokeStyle = "#272822";
-			ctx.fillStyle = "#66D9EF";
+			ctx.fillStyle = "#66D9EF"; // Blue
 		} else { 					// The cell stays dead
-			ctx.strokeStyle = "#272822";
-			ctx.fillStyle = "#272822";
+			ctx.fillStyle = "#272822"; // Gray
 		}
 		
 		ctx.rect(this.column * 10, this.row * 10, 10, 10);	// |
@@ -38,7 +35,7 @@ function Cell(r, c, p) {
 	Cell.prototype.showNeighborsCount = function() {
 		ctx.fillStyle = "#5d6052";
 		ctx.font = "10px Arial";
-		ctx.fillText(this.neighbors, this.column * 10 + 2, this.row * 10 - 2 + 10);
+		ctx.fillText(this.neighbors, this.column * 10 + 2, this.row * 10 - 2 + 10); // Drawing the number of neighbors
 	}
 
 	// This function counts the number of neighbors of the cell according to the number of rows and columns
@@ -62,7 +59,7 @@ function Cell(r, c, p) {
 
 	// This function updates the living state of the cell
 	Cell.prototype.updateState = function() {
-		this.wasAlive = this.alive;
+		this.wasAlive = this.alive; // Storing the "old" state
 
 		if(this.neighbors == 3) {		// The cell borns
 			this.alive = true;
