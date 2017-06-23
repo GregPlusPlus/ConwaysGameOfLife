fa64dd99ff318c5a16fea477b0028a3801659ef8
GregPlusPlus <bostgregoire@gmail.com>
2017-06-23 17:32:00 +0200

    Update utils.js

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,34 +1,38 @@
+// Initalizing some global variables
 var iterations = 0;
 var population = 0;
 var lastPopulation = 0;
 
+// This function counts/displays the population (alive cells)
 function countPopulation() {
-	population = 0;
+	population = 0; // Clear the population count
 
-	for(var i = 0 ; i < rows ; i++) {
-		for(var j = 0 ; j < columns ; j++) {
-			if(grid[i][j].alive) {
-				population++;
+	for(var i = 0 ; i < rows ; i++) {		// |- Scanning cells
+		for(var j = 0 ; j < columns ; j++) {	// |
+			if(grid[i][j].alive) {		// If the scanned cell is alive
+				population++;		// Then add 1 to the population count
 			}
 		}
 	}
 
-	document.getElementById("population").innerHTML = "Population: " + population;
+	var populationElmnt = document.getElementById("population"); // Gathering the 'population' element
 	
-	var populationElmnt = document.getElementById("population");
+	populationElmnt.innerHTML = "Population: " + population; // Displaying the population
 
-	if(population < lastPopulation) {
-		populationElmnt.style.color = "#fdb4cf";
-	} else if(population == lastPopulation) {
-		populationElmnt.style.color = "#dbdcd6";
-	} else if(population > lastPopulation) {
-		populationElmnt.style.color = "#e2f6bc";
+	// Colouring the text
+	if(population < lastPopulation) {			// The population is falling
+		populationElmnt.style.color = "#fdb4cf"; // Red
+	} else if(population == lastPopulation) {		// The population is stable
+		populationElmnt.style.color = "#dbdcd6"; // White
+	} else if(population > lastPopulation) {		// The population is rising
+		populationElmnt.style.color = "#e2f6bc"; // Green
 	}
 
-	lastPopulation = population;
+	lastPopulation = population; // Storing the population
 }
 
+// This function counts/displays the number of iterations
 function countIterations() {
-	iterations++;
-	document.getElementById("iterations").innerHTML = "Iterations: " + iterations;
+	iterations++; // adding 1 to the iterations count
+	document.getElementById("iterations").innerHTML = "Iterations: " + iterations; // Displaying the number of iterations
 }
